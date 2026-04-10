ae53c52560dec83b92ca08d084e6e90b5716c18b
Ojalsharma04 <ojalsharma.sh@gmail.com>
2026-04-10 14:46:49 -0500

    Add type hints to Televison class attributes and methods

--- televison.py
+++ televison.py
@@ -1,24 +1,32 @@
 class Televison:
 
-    MUTE_VOLUME = 0
-    MIN_VOLUME = 0
-    MAX_VOLUME = 2
-    MIN_CHANNEL = 0
-    MAX_CHANNEL = 3
+    MUTE_VOLUME: int = 0
+    MIN_VOLUME: int = 0
+    MAX_VOLUME: int = 2
+    MIN_CHANNEL: int = 0
+    MAX_CHANNEL: int = 3
 
     def __init__(self):
-        self.__status = False
-        self.__muted = False
-        self.__volume = Televison.MIN_VOLUME
-        self.__channel = Televison.MIN_CHANNEL
-        self.__prev_volume = Televison.MIN_VOLUME
+        self.__status : bool = False
+        self.__muted : bool = False
+        self.__volume: int = Televison.MIN_VOLUME
+        self.__channel: int = Televison.MIN_CHANNEL
+        self.__prev_volume: int = Televison.MIN_VOLUME
 
     def power(self):
+        """
+        Turns the TV status to true or false
+        :return: nothing
+        """
         self.__status = not self.__status
 
 
 
     def mute(self):
+        """
+        Makes the volume of the TV zero
+        :return: nothing
+        """
         if self.__status:
             if not self.__muted:
                 self.__prev_volume = self.__volume
@@ -30,6 +38,10 @@ class Televison:
 
 
     def channel_up(self):
+        """
+        Makes the channel go one up
+        :return: nothing
+        """
         if self.__status:
             if self.__channel < Televison.MAX_CHANNEL:
                 self.__channel += 1
@@ -38,6 +50,10 @@ class Televison:
         else:
             pass
     def channel_down(self):
+        """
+        Makes the channel go one down
+        :return: nothing
+        """
         if self.__status:
             if self.__channel == Televison.MIN_CHANNEL:
                 self.__channel = Televison.MAX_CHANNEL
@@ -47,6 +63,10 @@ class Televison:
             pass
 
     def volume_up(self):
+        """
+        Makes the volume go one up
+        :return: nothing
+        """
         if self.__status:
             if self.__muted:
                 self.__volume = self.__prev_volume
@@ -56,6 +76,10 @@ class Televison:
                 self.__volume += 1
 
     def volume_down(self):
+        """
+        Makes the volume go one down
+        :return: nothing
+        """
         if self.__status:
             if self.__muted:
                 self.__volume = self.__prev_volume
@@ -63,7 +87,12 @@ class Televison:
 
             if self.__volume > Televison.MIN_VOLUME:
                 self.__volume -= 1
+
     def __str__(self):
+        """
+        Returns the power, channel, and volume status of the TV
+
+        """
         return(f"Power - [{self.__status}], Channel - [{self.__channel}], Volume - [{self.__volume}]")
 
 
